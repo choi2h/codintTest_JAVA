369c2d9849579e388b989f3a187663b1f80a5d29
choiihwa <chldlghk@gmail.com>
2022-09-03 00:59:03 +0900

    단어 뒤집기 풀이

--- src/inflearn/ReverseWord.java
+++ src/inflearn/ReverseWord.java
@@ -1,20 +1,45 @@
 package inflearn;
 
+import java.sql.Array;
+import java.util.ArrayList;
 import java.util.Scanner;
 
+/*
+StringBuilder(s).reverse(); -> 뒤집어줌
+ */
 public class ReverseWord {
 
-    public void solution(int n, String[] str) {
+    public ArrayList<String> solution(int n, String[] str) {
+        ArrayList<String> list = new ArrayList<>();
 
         for(String s : str) {
+            /*
             int length = s.length();
             StringBuilder result = new StringBuilder();
             for(int i=length-1; i >= 0; i--) {
                 result.append(s.charAt(i));
             }
+            */
 
-            System.out.println(result);
+            /*
+            String result = new StringBuilder(s).reverse().toString();
+            list.add(result);
+            */
+
+            char[] c = s.toCharArray();
+            int lt = 0, rt = c.length-1;
+            while(lt<rt) {
+                char tmp=c[lt];
+                c[lt] = c[rt];
+                c[rt] = tmp;
+                lt++;
+                rt--;
+            }
+            String tmp = String.valueOf(c);
+            list.add(tmp);
         }
+
+        return list;
     }
 
     public static void main(String[] args) {
@@ -26,6 +51,8 @@ public class ReverseWord {
             str[i] = scanner.next();
         }
 
-        reverseWord.solution(n, str);
+        for(String s : reverseWord.solution(n, str)) {
+            System.out.println(s);
+        }
     }
 }
