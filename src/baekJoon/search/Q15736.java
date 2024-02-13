8c8384efbc6d41a6622e1776ac05d1ea18b2376b
choiihwa <chldlghk@gmail.com>
2024-02-13 21:31:33 +0900

    [Baekjoon] Q15736 청기백기 풀이

---
+++ src/baekJoon/search/Q15736.java
@@ -0,0 +1,28 @@
+package baekJoon.search;
+
+import java.util.Scanner;
+
+// 청기백기
+// https://www.acmicpc.net/problem/15736
+public class Q15736 {
+
+    // 제곱수를 구하는 문제
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        int count = 0;
+        for(int i=1; i<=n; i++) {
+            // 1부터 제곱수 구함
+            // 제곱수가 최대값 n보다 크면 범위 초과로 중단
+            if(Math.pow(i, 2) > n) {
+                break;
+            }
+
+            // 제곱수가 범위 안에 포함되면 백기로 판단하여 count 추가
+            count++;
+        }
+
+        System.out.println(count);
+    }
+}
