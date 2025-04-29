6a38982055158692d653f1ffd9a0b2c793b44968
Ihwa <84762486+choi2h@users.noreply.github.com>
2025-04-29 21:40:04 +0900

    [Silver II] Title: 스택 수열, Time: 312 ms, Memory: 25720 KB -BaekjoonHub

--- 백준/Silver/1874. 스택 수열/스택 수열.java
+++ 백준/Silver/1874. 스택 수열/스택 수열.java
@@ -1,37 +1,39 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+import java.util.Stack;
 
 public class Main {
-    public static String solutionWithBufferedReaderAndArray() throws IOException {
+
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int count = Integer.parseInt(br.readLine());
 
-        int num = 0;
-        int index = -1;
-        int[] array = new int[count];
-        StringBuilder sb = new StringBuilder();
-        for(int i=0; i<count; i++) {
-            int findNumber = Integer.parseInt(br.readLine());
-
-            while(array[0] == 0 || array[index] < findNumber) {
-                array[++index] = ++num;
-                sb.append("+").append("\n");
-            }
-
-            if(array[index] > findNumber) {
-                return "NO";
-            }
-
-            array[index--] = 0;
-            sb.append("-").append("\n");
+        int[] nums = new int[count];
+        for (int i = 0; i < count; i++) {
+            nums[i] = Integer.parseInt(br.readLine());
         }
 
-        return sb.toString();
-    }
-    
-    public static void main(String[] args) throws IOException {
-        String result = solutionWithBufferedReaderAndArray();
-        System.out.println(result);
+        int cur = 1, index = 0;
+        StringBuilder sb = new StringBuilder();
+        Stack<Integer> stack = new Stack<>();
+        while (index < nums.length) {
+            if (cur <= count && (stack.isEmpty() || stack.peek() < nums[index])) {
+                stack.push(cur++);
+                sb.append("+").append("\n");
+            } else if (!stack.isEmpty() && stack.peek() >= nums[index]) {
+                if(stack.peek() == nums[index]) index++;
+                stack.pop();
+                sb.append("-").append("\n");
+            } else {
+                break;
+            }
+        }
+
+        if(cur <= count || !stack.isEmpty() || index < nums.length) System.out.println("NO");
+        else System.out.println(sb);
     }
 }
