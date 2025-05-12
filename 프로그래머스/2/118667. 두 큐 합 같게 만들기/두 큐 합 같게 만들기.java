c04acf89da5e5815727d4d7bce8600c1925f4c0d
Ihwa <84762486+choi2h@users.noreply.github.com>
2025-05-12 17:50:41 +0900

    [level 2] Title: 두 큐 합 같게 만들기, Time: 69.82 ms, Memory: 119 MB -BaekjoonHub

---
+++ 프로그래머스/2/118667. 두 큐 합 같게 만들기/두 큐 합 같게 만들기.java
@@ -0,0 +1,37 @@
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+class Solution {
+    public int solution(int[] queue1, int[] queue2) {
+        int n = queue1.length;
+        Deque<Integer> q1 = new ArrayDeque();
+        Deque<Integer> q2 = new ArrayDeque();
+        long tot1 = 0, tot2 = 0;
+        for(int i = 0; i < n; i++){
+            tot1 += queue1[i];
+            q1.add(queue1[i]);
+        }
+        for(int i = 0; i < n; i++){
+            tot2 += queue2[i];
+            q2.add(queue2[i]);
+        }
+
+        for(int i = 0; i < 4*n + 1; i++){
+            if(tot1 == tot2) return i;
+            if(tot1 < tot2){
+                int x = q2.removeFirst();
+                tot1 += x;
+                tot2 -= x;
+                q1.add(x);
+            }
+            else{
+                int x = q1.removeFirst();
+                tot2 += x;
+                tot1 -= x;
+                q2.add(x);
+            }
+        }
+
+        return -1;
+    }
+}
