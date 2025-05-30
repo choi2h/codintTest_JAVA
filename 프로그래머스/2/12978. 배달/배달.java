32da8c218940dc01132a706d2fbfe3540d5e2061
Ihwa <84762486+choi2h@users.noreply.github.com>
2025-05-30 22:29:13 +0900

    [level 2] Title: 배달, Time: 4.66 ms, Memory: 84.3 MB -BaekjoonHub

--- 프로그래머스/2/12978. 배달/배달.java
+++ 프로그래머스/2/12978. 배달/배달.java
@@ -1,10 +1,53 @@
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Queue;
+import java.util.LinkedList;
+
 class Solution {
+    class Node {
+        int num;
+        int time;
+        
+        public Node(int num, int time) {
+            this.num = num;
+            this.time = time;
+        }
+    }
+    
     public int solution(int N, int[][] road, int K) {
-        int answer = 0;
-
-        // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
-        System.out.println("Hello Java");
-
+        List<Node>[] arr = new ArrayList[N+1]; 
+        for(int i=0; i<arr.length; i++) {
+            arr[i] = new ArrayList<>();
+        }
+        
+        for(int i=0; i<road.length; i++) {
+            int start = road[i][0];
+            int end = road[i][1];
+            int time = road[i][2];
+            
+            arr[start].add(new Node(end, time));
+            arr[end].add(new Node(start, time));
+        }
+        
+        int[] times = new int[N+1];
+        Queue<Integer> queue = new LinkedList<>();
+        queue.add(1);
+        while(!queue.isEmpty()) {
+            int cur = queue.poll();
+            
+            for(Node next : arr[cur]) {
+                int sumTime = times[cur] + next.time;
+                if(times[next.num] > 0 && times[next.num] <= sumTime) continue;
+                times[next.num] = sumTime;
+                queue.add(next.num);
+            }
+        }
+        
+        int answer = 1;
+        for(int i=2; i<times.length; i++) {
+            if(times[i]>0 && times[i] <= K) answer++;
+        }
+        
         return answer;
     }
 }
