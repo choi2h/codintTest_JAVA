4f18d5f53b115cde4c38234ac2dc09eaef60f812
Ihwa <84762486+choi2h@users.noreply.github.com>
2025-06-19 23:36:11 +0900

    [Gold III] Title: 개구리 점프, Time: 784 ms, Memory: 81396 KB, Score: 100 point -BaekjoonHub

--- 백준/Gold/17619. 개구리 점프/개구리 점프.java
+++ 백준/Gold/17619. 개구리 점프/개구리 점프.java
@@ -1,66 +1,76 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class Main {
+    private static class Node {
+        int idx;
+        int x1;
+        int x2;
+        int y;
+
+        Node(int idx, int x1, int x2, int y) {
+            this.idx = idx;
+            this.x1 = x1;
+            this.x2 = x2;
+            this.y = y;
+        }
+    }
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         int n = Integer.parseInt(st.nextToken());
         int q = Integer.parseInt(st.nextToken());
 
-        int[][] arr = new int[n+1][3];
+        Node[] arr = new Node[n+1];
+        arr[0] = new Node(0, 0, 0, 0);
+        int[] records = new int[n+1];
         for(int i=1; i<=n; i++) {
             st = new StringTokenizer(br.readLine());
-            arr[i][0] = Integer.parseInt(st.nextToken());
-            arr[i][1] = Integer.parseInt(st.nextToken());
-            arr[i][2] = Integer.parseInt(st.nextToken());
+            int x1 = Integer.parseInt(st.nextToken());
+            int x2 = Integer.parseInt(st.nextToken());
+            int y = Integer.parseInt(st.nextToken());
+            arr[i] = new Node(i, x1, x2, y);
+            records[i] = i;
         }
 
+        Arrays.sort(arr, (a, b) -> a.x1 == b.x1 ? a.x2-b.x2 : a.x1-b.x1 );
+
+        int maxRight = arr[1].x2;
         StringBuilder sb = new StringBuilder();
-        boolean[][] visited = new boolean[n+1][n+1];
+        for(int i=2; i<arr.length; i++) {
+            if(arr[i].x1 <= maxRight) {
+                union(records, arr[i-1].idx, arr[i].idx);
+                maxRight = Math.max(arr[i].x2, maxRight);
+            } else {
+                maxRight = arr[i].x2;
+            }
+        }
+
         for(int i=1; i<=q; i++) {
             st = new StringTokenizer(br.readLine());
             int start = Integer.parseInt(st.nextToken());
             int end = Integer.parseInt(st.nextToken());
 
-            if(!visited[start][start] || visited[end][start]) {
-                find(arr, visited, start);
-            }
-
-            int result = visited[start][end] || visited[end][start]? 1: 0;
+            int result = find(records, start) == find(records, end) ? 1 : 0;
             sb.append(result).append("\n");
         }
 
         System.out.println(sb);
     }
 
-    private static void find(int[][] arr, boolean[][] visited, int start) {
-        Queue<Integer> queue = new LinkedList<>();
-        queue.add(start);
+    private static void union(int[] record, int a, int b) {
+        int fa = find(record, a);
+        int fb = find(record, b);
 
-        while(!queue.isEmpty()) {
-            int cur = queue.poll();
-
-            if(visited[start][cur]) continue;
-            visited[start][cur] = true;
-
-            for(int i=0; i< arr.length; i++) {
-                if(visited[start][i]) continue;
-                if(canMove(arr[cur], arr[i])) {
-                    queue.add(i);
-                }
-            }
-        }
+        if(fa == fb) return;
+        record[fa] = fb;
     }
 
-    private static boolean canMove(int[] a, int[] b) {
-        return (a[0] >= b[0] && a[0] <= b[1])
-                || (a[1] >= b[0] && a[1] <= b[1])
-                || (b[0] >= a[0] && b[0] <= a[1])
-                || (b[1] >= a[0] && b[1] <= a[1]);
+    private static int find(int[] record, int n) {
+        if(record[n] == n) return n;
+        return record[n] = find(record, record[n]);
     }
 }
