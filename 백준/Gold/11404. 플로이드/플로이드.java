dbdc9b93a9c2f4ac032a311bccf66e0b43ce139d
Ihwa <84762486+choi2h@users.noreply.github.com>
2025-09-05 12:41:54 +0900

    [Gold IV] Title: 플로이드, Time: 392 ms, Memory: 43656 KB -BaekjoonHub

--- 백준/Gold/11404. 플로이드/플로이드.java
+++ 백준/Gold/11404. 플로이드/플로이드.java
@@ -1,64 +1,59 @@
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Comparator;
-import java.util.List;
-import java.util.PriorityQueue;
+import java.io.IOException;
 import java.util.StringTokenizer;
 
 public class Main {
+
+    private static final int MAX_VALUE = 10_000_001;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
         int m = Integer.parseInt(br.readLine());
-        List<int[]>[] buses = new ArrayList[n + 1];
-        int[][] dp = new int[n + 1][n + 1];
-        for (int i = 0; i <= n; i++) {
-            buses[i] = new ArrayList<>();
-            Arrays.fill(dp[i], Integer.MAX_VALUE);
+
+        int[][] map = new int[n+1][n+1];
+        for(int i=1; i<=n; i++) {
+            for(int j=1; j<=n; j++) {
+                map[i][j] = MAX_VALUE;
+            }
         }
 
         StringTokenizer st;
-        for (int i = 0; i < m; i++) {
+        for(int i=0; i<m; i++) {
             st = new StringTokenizer(br.readLine());
             int a = Integer.parseInt(st.nextToken());
             int b = Integer.parseInt(st.nextToken());
             int c = Integer.parseInt(st.nextToken());
 
-            buses[a].add(new int[]{b, c});
+            map[a][b] = Math.min(map[a][b], c);
         }
 
-        PriorityQueue<int[]> pq = new PriorityQueue<>(Comparator.comparingInt(a -> a[1]));
-        for (int i = 1; i <= n; i++) {
-            boolean[] visited = new boolean[n + 1];
-            pq.add(new int[]{i, 0});
-            while (!pq.isEmpty()) {
-                int[] cur = pq.poll();
-                int node = cur[0];
-                int cost = cur[1];
+        floydwarshall(n, map);
+        print(n, map);
+    }
 
-                if (visited[node]) continue;
-                visited[node] = true;
-                for (int[] bus : buses[node]) {
-                    int next = bus[0];
-                    int nextCost = cost + bus[1];
-
-                    if(next == i || visited[next]) continue;
-                    dp[i][next] = Math.min(dp[i][next], nextCost);
-                    pq.add(new int[]{next, nextCost});
+    private static void floydwarshall(int n, int[][] map) {
+        for(int k=1; k<=n; k++) {
+            for(int i=1; i<=n; i++) {
+                for(int j=1; j<=n; j++) {
+                    if(map[i][j] > map[i][k] + map[k][j]) {
+                        map[i][j] = map[i][k] + map[k][j];
+                    }
                 }
             }
         }
+    }
 
+    private static void print(int n, int[][] map) {
         StringBuilder sb = new StringBuilder();
+
         for(int i=1; i<=n; i++) {
-            int[] costs = dp[i];
             for(int j=1; j<=n; j++) {
-                int cost = costs[j];
-                sb.append(cost == Integer.MAX_VALUE ? 0 : cost).append(" ");
+                if(i==j || map[i][j] == MAX_VALUE) sb.append(0);
+                else sb.append(map[i][j]);
+
+                sb.append(" ");
             }
             sb.append("\n");
         }
