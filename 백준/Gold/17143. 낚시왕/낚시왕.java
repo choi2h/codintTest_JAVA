e129f8c1b88b2c4aa8ce19ee1ee1ba95b90e1b3e
Ihwa <84762486+choi2h@users.noreply.github.com>
2025-09-15 16:32:29 +0900

    [Gold I] Title: 낚시왕, Time: 268 ms, Memory: 22480 KB -BaekjoonHub

--- 백준/Gold/17143. 낚시왕/낚시왕.java
+++ 백준/Gold/17143. 낚시왕/낚시왕.java
@@ -2,14 +2,11 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
+import java.util.Arrays;
 
 public class Main {
 
-    private static int[][] move = {{0,0}, {-1,0}, {1,0}, {0,1}, {0,-1}};
-    private static int[] width; 
-    private static int[] height; 
-    private static int R;
-    private static int C;
+    private static int R, C, RCycle, CCycle;
 
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -18,19 +15,10 @@ public class Main {
         C = Integer.parseInt(st.nextToken());
         int M = Integer.parseInt(st.nextToken());
 
-        width = new int[C==1 ? 1 : C*2-2];
-        height = new int[R==1 ? 1 : R*2-2];
-
-        for (int i=0; i<width.length; i++) {
-            width[i] = i>=C ? C-(i-C)-2 : i;
-        }
-
-        for(int i=0; i<height.length; i++) {
-            height[i] = i>=R ? R-(i-R)-2 : i;
-        }
-
         int[][] arr = new int[R][C];
         Shark[] sharks = new Shark[M+1];
+        RCycle = 2 * (R-1);
+        CCycle = 2 * (C-1);
         for(int i=1; i<=M; i++) {
             st = new StringTokenizer(br.readLine());
             int r= Integer.parseInt(st.nextToken());
@@ -39,7 +27,9 @@ public class Main {
             int d= Integer.parseInt(st.nextToken());
             int z= Integer.parseInt(st.nextToken());
 
-            s = d == 1 || d ==2 ? s%((R-1)*2) : s%((C-1)*2);
+            if(d <= 2) s = s%RCycle;
+            else s = s%CCycle;
+
             Shark shark = new Shark(r-1, c-1, s, d, z);
             arr[r-1][c-1] = i;
             sharks[i] = shark;
@@ -50,60 +40,55 @@ public class Main {
             // 상어 잡기
             answer += getShark(arr, sharks, i);
             
-            int[][] newArr = new int[R][C];
-            
             // 상어 이동
+            resetArray(arr);
             for(int j=1; j<=M; j++) {
                 if(sharks[j] == null) continue;
-                moveShark(arr, newArr, sharks, j);
+                moveShark(arr, sharks, j);
             }
-
-            arr = newArr;
         }
 
         System.out.print(answer);
     }
 
-    private static void moveShark(int[][] arr, int[][] newArr, Shark[] sharks, int cur) {
-        Shark shark = sharks[cur];
-        shark.move();
-        if(newArr[shark.r][shark.c] > 0) removeDuplicationShark(newArr, sharks, shark.r, shark.c, cur);
-        else newArr[shark.r][shark.c] = cur;
+    private static void resetArray(int[][] arr) {
+        for(int[] a : arr) {
+            Arrays.fill(a, 0);
+        }
     }
 
-    private static int getShark(int[][] arr, Shark[] sharks, int king) {
-        for(int k=0; k<R; k++) {
-            if(arr[k][king] > 0) {
-                int index = arr[k][king];
-                int size = sharks[index].z;
-                sharks[index] = null;
-                arr[k][king] = 0;
-                return size;
-            }
+    private static void moveShark(int[][] arr, Shark[] sharks, int cur) {
+        Shark shark = sharks[cur];
+        shark.move();
+        if(arr[shark.r][shark.c] == 0) arr[shark.r][shark.c] = cur;
+        else removeDuplicationShark(arr, sharks, shark.r, shark.c, cur);
+    }
+
+    private static int getShark(int[][] arr, Shark[] sharks, int c) {
+        for(int r=0; r<R; r++) {
+            if(arr[r][c] == 0) continue;
+
+            int index = arr[r][c];
+            int size = sharks[index].z;
+            sharks[index] = null;
+            return size;
         }
 
         return 0;
     }
 
     private static void removeDuplicationShark(int[][] arr, Shark[] sharks, int r, int c, int cur) {
-        Shark shark = sharks[cur];
-        int sharkIndex = arr[r][c];
-        Shark shark2 = sharks[sharkIndex];
+        int shark1Size = sharks[cur].z;
+        int shark2Size = sharks[arr[r][c]].z;
 
-        if(shark.z > shark2.z) {
-            arr[shark.r][shark.c] = cur;
-            sharks[sharkIndex] = null;
-        } else if(shark.z < shark2.z) {
-            sharks[cur] = null;
-        }
+        if(shark1Size > shark2Size) {
+            sharks[arr[r][c]] = null;
+            arr[r][c] = cur;
+        } else sharks[cur] = null;
     }
 
     private static class Shark {
-        int r;
-        int c;
-        int s;
-        int d;
-        int z;
+        int r, c, s, d, z;
 
         Shark(int r, int c, int s, int d, int z) {
             this.r = r;
@@ -115,23 +100,27 @@ public class Main {
 
         // 1 위 2 아래 3 오른쪽 4 왼쪽
         void move() {        
-            int[] moveBase = move[d];
-            int rIndex = r+(s*moveBase[0]);
-            int cIndex = c+(s*moveBase[1]);
-
-            rIndex = rIndex == 0 ? rIndex : rIndex%height.length;
-            cIndex = cIndex == 0 ? cIndex : cIndex%width.length;
-
-            rIndex = rIndex >= 0 ? rIndex : height.length+rIndex;
-            cIndex = cIndex >= 0 ? cIndex : width.length+cIndex;
-
-            r = height[rIndex];
-            c = width[cIndex];
-
-            if(d==1) d = rIndex<R ? 1: 2;
-            else if(d==2) d =  rIndex < R ? 2 : 1;
-            else if(d==3) d =  cIndex < C ? 3 : 4;
-            else if(d==4) d =  cIndex < C ? 4 : 3;
+            if (d <= 2) {
+              int currentPos = (d == 1) ? RCycle - r : r;
+              currentPos = (currentPos + s) % RCycle;
+              if (currentPos >= R) {
+                  this.r = RCycle - currentPos;
+                  this.d = 1;
+              } else {
+                  this.r = currentPos;
+                  this.d = 2;
+              }
+          } else { 
+             int currentPos = (d == 4) ? CCycle - c : c;
+              currentPos = (currentPos + s) % CCycle;
+              if (currentPos >= C) {
+                  this.c = CCycle - currentPos;
+                  this.d = 4;
+              } else {
+                  this.c = currentPos;
+                  this.d = 3;
+              }
+          }
         }
     }
 }
