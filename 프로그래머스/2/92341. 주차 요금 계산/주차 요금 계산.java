ce712b3305bf94f317ded3c9d6de57e98456ae10
Ihwa <84762486+choi2h@users.noreply.github.com>
2025-10-02 00:59:34 +0900

    [level 2] Title: 주차 요금 계산, Time: 7.24 ms, Memory: 93.7 MB -BaekjoonHub

--- 프로그래머스/2/92341. 주차 요금 계산/주차 요금 계산.java
+++ 프로그래머스/2/92341. 주차 요금 계산/주차 요금 계산.java
@@ -1,71 +1,38 @@
 import java.util.Map;
-import java.util.HashMap;
-import java.util.List;
-import java.util.ArrayList;
-import java.util.Collections;
+import java.util.TreeMap;
 
 class Solution {
-    private static final String IN_SIGN = "IN";
-    private static final String OUT_SIGN = "OUT";
-    private static final int[] MAX_TIME = new int[]{23, 59};
+    
+    private static final int MAX_TIME = (23*60) + 59;
     
     public int[] solution(int[] fees, String[] records) {
-        int baseTime = fees[0];
-        int baseFee = fees[1];
-        int unitTime = fees[2];
-        int unitFee = fees[3];
-        
-        Map<String, Integer> sumRecords = new HashMap<>();
-        Map<String, int[]> inRecords = new HashMap<>();
+        Map<String, Integer> map = new TreeMap<>();
         for(String record : records) {
             String[] recordInfo = record.split(" ");
-            int[] time = convertStringToIntArr(recordInfo[0]);
+            int sign = recordInfo[2].equals("IN") ? -1 : 1;
+            int time = getTime(recordInfo[0]) * sign;
             String num = recordInfo[1];
-            String sign = recordInfo[2];
-            
-            if(sign.equals(IN_SIGN)) inRecords.put(num, time);
-            else {
-                int timeInterval = getTimeInterval(inRecords.get(num), time);
-                sumRecords.put(num, sumRecords.getOrDefault(num, 0) + timeInterval);
-                inRecords.remove(num);
-            }
+            map.put(num, map.getOrDefault(num, 0) + time);
         }
-        
-        for (String num : inRecords.keySet()) {
-            int timeInterval = getTimeInterval(inRecords.get(num), MAX_TIME);
-            sumRecords.put(num, sumRecords.getOrDefault(num, 0) + timeInterval);
-        }
-        
-        List<String> nums = new ArrayList<>(sumRecords.keySet());
-        Collections.sort(nums);
-        int[] answer = new int[sumRecords.size()];
-        for (int i=0; i<nums.size(); i++) {
-            int time = sumRecords.get(nums.get(i));
-            answer[i] += baseFee;
-            
-            if(time > baseTime) {
-                int overTime = time-baseTime;
-                answer[i] += ((overTime/unitTime) + (overTime%unitTime > 0 ? 1 : 0))*unitFee;
+    
+        int idx = 0;
+        int[] answer = new int[map.size()];
+        for (int time : map.values()) {
+            if(time <= 0) time += MAX_TIME;
+            int cost = fees[1];
+            if(time > fees[0]) {
+                time -= fees[0];
+                cost += ((time/fees[2]) + (time%fees[2] > 0 ? 1 : 0))*fees[3];
             }
+            answer[idx++] = cost;
         }
         
         
         return answer;
     }
     
-    private static int[] convertStringToIntArr(String time) {
+    private static int getTime(String time) {
         String[] times = time.split(":");
-        int[] arr = new int[2];
-        for(int i=0; i<2; i++) {
-            arr[i] = Integer.parseInt(times[i]);
-        }
-        
-        return arr;
-    }
-    
-    private static int getTimeInterval(int[] before, int[] after) {
-        int hour = after[0]-(before[0]+1);
-        int time = (60-before[1])+after[1];
-        return (hour*60) + time;
+        return (Integer.parseInt(times[0])*60) + Integer.parseInt(times[1]);
     }
 }
