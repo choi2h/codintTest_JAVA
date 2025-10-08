daee04b9e80b03ca9668a88df0e251b6d9ee532b
Ihwa <84762486+choi2h@users.noreply.github.com>
2025-10-08 22:30:58 +0900

    [level 3] Title: 미로 탈출 명령어, Time: 1.00 ms, Memory: 76.5 MB -BaekjoonHub

--- 프로그래머스/3/150365. 미로 탈출 명령어/미로 탈출 명령어.java
+++ 프로그래머스/3/150365. 미로 탈출 명령어/미로 탈출 명령어.java
@@ -1,46 +1,30 @@
 class Solution {
-    private static final int[][] MOVE = {{1, 0}, {0, -1}, {0, 1}, {-1, 0}};
-    private static final char[] MOVE_MESSAGE = {'d', 'l', 'r', 'u'};
-    private static boolean[][] checked;
-    
     public String solution(int n, int m, int x, int y, int r, int c, int k) {
         int maxMoveCount = Math.abs(r-x) + Math.abs(c-y);
-        if(maxMoveCount%2 != k%2) return "impossible";
+        if(maxMoveCount%2 != k%2 || maxMoveCount > k) return "impossible";
         
-        int[] table = new int[]{n, m};
-        int[] end = new int[]{r, c};
-        checked = new boolean[n+1][m+1];
-        
-        String answer = find(table, x, y, end, 0, k);
-        if(answer.isEmpty()) return "impossible";
-        return answer;
-    }
-    
-    private String find
-        (int[] table, int x, int y, int[] arrived, int count, int max) {
-        if(checked[x][y] || 
-            Math.abs(arrived[0]-x) + Math.abs(arrived[1]-y) > max-count) {
-            checked[x][y] = true;
-            return "";
-        }
-        
-        for(int i=0; i<MOVE.length; i++) {
-            int mx = x+MOVE[i][0];
-            int my = y+MOVE[i][1];
-            if(!canMove(table, mx, my)) continue;
-            if(count+1 < max && !checked[mx][my]) {
-                String result = MOVE_MESSAGE[i] + 
-                    find(table, mx, my, arrived, count+1, max);
-                if(result.length() == max-count) return result;
-            } else if(count+1 == max && mx == arrived[0] && my == arrived[1]) {
-                return  "" + MOVE_MESSAGE[i];
+        StringBuilder sb = new StringBuilder();
+        while(k-- > 0) {
+            int dx = x+1, ly = y-1, ry = y+1, ux = x-1;
+            if(dx <= n && canMove(dx, y, r, c, k)) {
+                x = dx;
+                sb.append('d');
+            } else if(ly > 0 && canMove(x, ly, r, c, k)) {
+                y = ly;
+                sb.append('l');
+            } else if(ry <= m && canMove(x, ry, r, c, k)) {
+                y = ry;
+                sb.append('r');
+            } else if(ux > 0 && canMove(ux, y, r, c, k)) {
+                x = ux;
+                sb.append('u');
             }
         }
         
-        return "";
+        return sb.toString();
     }
     
-    private boolean canMove(int[] table, int x, int y) {
-        return x > 0 && x <= table[0] && y > 0 && y <= table[1];
+    private boolean canMove(int x, int y, int r, int c, int count) {
+        return Math.abs(r-x) + Math.abs(c-y) <= count;
     }
 }
