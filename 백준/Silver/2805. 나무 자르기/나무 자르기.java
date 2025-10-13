12e41e6c90b37c1ba8d985aa6fefacdd6f57ddb6
Ihwa <84762486+choi2h@users.noreply.github.com>
2025-10-13 17:35:51 +0900

    [Silver II] Title: 나무 자르기, Time: 1028 ms, Memory: 124556 KB -BaekjoonHub

--- 백준/Silver/2805. 나무 자르기/나무 자르기.java
+++ 백준/Silver/2805. 나무 자르기/나무 자르기.java
@@ -1,40 +1,48 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 import java.util.Arrays;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
+
+    public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String[] input1 = br.readLine().split(" ");
-        int n = Integer.parseInt(input1[0]);
-        long m = Long.parseLong(input1[1]);
-        String[] input2 = br.readLine().split(" ");
-        long[] arr = new long[n];
-        long max = 0, min = 0;
-        for(int i=0; i<input2.length; i++){
-            arr[i] = Long.parseLong(input2[i]);
-            max = Math.max(max, arr[i]);
-            min = Math.min(min, arr[i]);
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        long m = Long.parseLong(st.nextToken());
+
+        long[] trees = new long[n];
+        st = new StringTokenizer(br.readLine());
+        for(int i=0; i<n; i++) {
+            trees[i] = Long.parseLong(st.nextToken());
         }
+        
+        Arrays.sort(trees);
 
-        Arrays.sort(arr);
-        long answer = 0;
-        while(max >= min) {
-            long mid=(max+min)/2;
-            long sum = 0;
+        long answer=0;
+        long l=0, r=trees[n-1], mid=0;
+        while(l<=r) {
+            mid = (l+r)/2;
 
-            for(int i=arr.length-1; i>=0; i--){
-                if(arr[i] <= mid) break;
-                sum += arr[i]-mid;
-            }
-
+            long sum = getTotalHeight(trees, mid);
             if(sum >= m) {
-                answer = mid;
-                min = mid+1;
-            } else max = mid-1;
+                answer = Math.max(answer, mid);
+                l = mid+1;
+            } else r = mid-1;
         }
 
-        System.out.println(answer);
+        System.out.print(answer);
+    }
+
+    private static long getTotalHeight(long[] trees, long cut) {
+        long sum = 0;
+        for(int i=trees.length-1; i>=0; i--) {
+            if(trees[i] <= cut) break;
+
+            sum += trees[i]-cut;
+        }
+
+        return sum;
     }
 }
