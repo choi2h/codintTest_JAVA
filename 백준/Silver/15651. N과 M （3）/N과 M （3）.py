98ff5afe0bc20fe6c13ed65f7f3fea1f055cd93a
Ihwa <84762486+choi2h@users.noreply.github.com>
2025-11-14 17:04:57 +0900

    [Silver III] Title: N과 M (3), Time: 812 ms, Memory: 113672 KB -BaekjoonHub

--- 백준/Silver/15651. N과 M （3）/N과 M （3）.py
+++ 백준/Silver/15651. N과 M （3）/N과 M （3）.py
@@ -1,22 +1,13 @@
 import sys
-
-result = []
+from itertools import product
 
 def solve():
     N, M = map(int, sys.stdin.readline().split())
-    record = [0] * M
-    dfs(N, M, record, 0)
+    result = []
+    for p in product(range(1, N+1), repeat=M):
+        result.append(" ".join(map(str, p)))
     answer = '\n'.join(result)
     print(answer)
-
-
-def dfs(N, M, cur, cnt):
-    for i in range(1, N+1):
-        cur[cnt] = i
-        if cnt+1 == M:
-            result.append(" ".join(map(str, cur)))
-        else :
-            dfs(N, M, cur, cnt+1)
 
 if __name__ == "__main__":
     solve()
