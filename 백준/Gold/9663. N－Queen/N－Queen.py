c170edbb69269bd6055ab35196b09f58a46ce0b9
Ihwa <84762486+choi2h@users.noreply.github.com>
2025-11-27 00:28:32 +0900

    [Gold IV] Title: N-Queen, Time: 25748 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Gold/9663. N－Queen/N－Queen.py
+++ 백준/Gold/9663. N－Queen/N－Queen.py
@@ -9,18 +9,18 @@ def solve():
     total_cnt = 0
     def dfs(x):
         nonlocal total_cnt
+        if x == N:
+            total_cnt += 1
+            return;
         
         for y in range(N):
             cl, cr = x+y, x-y+N
             if check_y[y] or check_left[cl] or check_right[cr]:
                 continue
             
-            check_y[y], check_left[cl], check_right[cr] = True, True, True
-            if x == N-1:
-                total_cnt += 1
-            else:
-                dfs(x+1)
-            check_y[y], check_left[cl], check_right[cr] = False, False, False
+            check_y[y] = check_left[cl] = check_right[cr] = True
+            dfs(x+1)
+            check_y[y] = check_left[cl] = check_right[cr] = False
     
     dfs(0)
     return total_cnt
