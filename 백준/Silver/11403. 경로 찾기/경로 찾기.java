2c0974a221e1d5c3d3ec0671abb25b3799e5b002
Ihwa <84762486+choi2h@users.noreply.github.com>
2026-01-13 17:43:37 +0900

    [Silver I] Title: 경로 찾기, Time: 152 ms, Memory: 16032 KB -BaekjoonHub

--- 백준/Silver/11403. 경로 찾기/경로 찾기.java
+++ 백준/Silver/11403. 경로 찾기/경로 찾기.java
@@ -6,6 +6,7 @@ import java.util.StringTokenizer;
 
 public class Main {
 
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
@@ -19,55 +20,22 @@ public class Main {
             }
         }
 
-        boolean[] visited = new boolean[n+1];
-        int[][] result = new int[n+1][n+1];
-        StringBuilder sb = new StringBuilder();
-        for (int i=1; i<=n; i++){
-            find(i, visited, nodes);
-
-            for (int j=1; j<=n; j++) {
-                if (i==j) continue;
-                if (visited[j]) {
-                    result[i][j] = 1;
-
-                    if (nodes[j][i] == 1) result[i][i] = 1;
+        for (int k=1; k<=n; k++) {
+            for (int i=1; i<=n; i++) {
+                for (int j=1; j<=n; j++) {
+                    nodes[i][j] = nodes[i][j] == 1 ? 1 : nodes[i][k] == 1 && nodes[k][j] == 1 ? 1 : 0;
                 }
             }
-
-            reset(visited);
         }
 
+        StringBuilder sb = new StringBuilder();
         for (int i=1; i<=n; i++) {
             for (int j=1; j<=n; j++) {
-                sb.append(result[i][j]).append(" ");
+                sb.append(nodes[i][j]).append(" ");
             }
             sb.append("\n");
         }
 
         System.out.println(sb);
-    }
-
-    private static void find(int n, boolean[] visited, int[][] nodes) {
-        PriorityQueue<Integer> queue = new PriorityQueue<>();
-        queue.add(n);
-
-        while(!queue.isEmpty()) {
-            int cur = queue.poll();
-
-            if (visited[cur]) continue;
-            visited[cur] = true;
-
-            for (int i=1; i<nodes.length; i++) {
-                if (nodes[cur][i] == 1 && !visited[i]) {
-                    queue.add(i);
-                }
-            }
-        }
-    }
-
-    private static void reset(boolean[] visited) {
-        for(int i=1; i<visited.length; i++) {
-            visited[i] = false;
-        }
     }
 }
