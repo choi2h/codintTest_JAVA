c26b38eb80e1662511293fed34e74ff75922db18
Ihwa <84762486+choi2h@users.noreply.github.com>
2026-01-20 12:07:02 +0900

    [Silver I] Title: 케빈 베이컨의 6단계 법칙, Time: 112 ms, Memory: 14692 KB -BaekjoonHub

--- 백준/Silver/1389. 케빈 베이컨의 6단계 법칙/케빈 베이컨의 6단계 법칙.java
+++ 백준/Silver/1389. 케빈 베이컨의 6단계 법칙/케빈 베이컨의 6단계 법칙.java
@@ -1,13 +1,11 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.HashMap;
-import java.util.HashSet;
 import java.util.LinkedList;
-import java.util.Map;
+import java.util.List;
 import java.util.Queue;
-import java.util.Set;
 import java.util.StringTokenizer;
 
 public class Main {
@@ -18,69 +16,64 @@ public class Main {
         int N = Integer.parseInt(st.nextToken());
         int M = Integer.parseInt(st.nextToken());
 
-        Map<Integer, Set<Integer>> friends = new HashMap<>();
-        Map<Integer, Integer> counts = new HashMap<>();
+        List<Integer>[] friends = new ArrayList[N+1];
+        for (int i=1; i<N+1; i++) {
+            friends[i] = new ArrayList<>();
+        }
+
+        int[] counts = new int[N+1];
         for (int i=0; i<M; i++) {
             st = new StringTokenizer(br.readLine());
             int a = Integer.parseInt(st.nextToken());
             int b = Integer.parseInt(st.nextToken());
 
-            addFriend(friends, a, b);
-            addFriend(friends, b, a);
-            if (!counts.containsKey(a)) counts.put(a, 0);
-            if (!counts.containsKey(b)) counts.put(b, 0);
+            friends[a].add(b);
+            friends[b].add(a);
         }
 
         int minFriend = 0;
         int minCount = Integer.MAX_VALUE;
         boolean[] visited = new boolean[N+1];
         Queue<Integer> nodeQueue = new LinkedList<>();
-        for (int key : counts.keySet()) {
-            visited[key] = true;
-            find(key, friends, nodeQueue, counts, visited);
+        for (int i=1; i<=N; i++) {
+            visited[i] = true;
+            find(i, friends, nodeQueue, counts, visited);
             nodeQueue.clear();
 
-            int sum = sumAndReset(counts);
+            int sum = sum(counts);
             if (sum < minCount) {
                 minCount = sum;
-                minFriend = key;
-            } if (sum == minCount) minFriend = Math.min(minFriend, key);
-
+                minFriend = i;
+            }
+            Arrays.fill(counts, 0);
             Arrays.fill(visited, false);
         }
 
         System.out.println(minFriend);
     }
 
-    private static void find(int n, Map<Integer, Set<Integer>> friends,
-            Queue<Integer> nodeQueue, Map<Integer, Integer> counts, boolean[] visited) {
+    private static void find(int n, List<Integer>[] friends,
+            Queue<Integer> nodeQueue, int[] counts, boolean[] visited) {
         nodeQueue.add(n);
 
         while(!nodeQueue.isEmpty()) {
             int cur = nodeQueue.poll();
 
-            int curCnt = counts.get(cur);
-            for (int next : friends.get(cur)) {
-                if (visited[next] || next == n) continue;
+            int curCnt = counts[cur];
+            for (int next : friends[cur]) {
+                if (visited[next]) continue;
 
-                if (counts.get(next) == 0) counts.put(next, curCnt+1);
+                if (counts[next] == 0) counts[next] = curCnt+1;
                 visited[next] = true;
                 nodeQueue.add(next);
             }
         }
     }
 
-    private static void addFriend(Map<Integer, Set<Integer>> friends, int a, int b) {
-        Set<Integer> aFriends = friends.getOrDefault(a, new HashSet<>());
-        aFriends.add(b);
-        friends.put(a, aFriends);
-    }
-
-    private static int sumAndReset(Map<Integer, Integer> counts) {
+    private static int sum(int[] counts) {
         int sum = 0;
-        for (int key : counts.keySet()) {
-            sum += counts.get(key);
-            counts.put(key, 0);
+        for (int cnt : counts) {
+            sum += cnt;
         }
 
         return sum;
