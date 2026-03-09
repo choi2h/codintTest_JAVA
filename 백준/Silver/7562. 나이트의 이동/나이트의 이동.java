4dae003a95a6ef89887d447ea8981138d4e92c04
Ihwa <84762486+choi2h@users.noreply.github.com>
2026-03-09 11:54:04 +0900

    [Silver I] Title: 나이트의 이동, Time: 392 ms, Memory: 89536 KB -BaekjoonHub

---
+++ 백준/Silver/7562. 나이트의 이동/나이트의 이동.java
@@ -0,0 +1,67 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    private static Queue<Integer> xQueue = new LinkedList<>();
+    private static Queue<Integer> yQueue = new LinkedList<>();
+    private static int[] MOVE_X = {-2, -2, -1, -1, 1, 1, 2, 2};
+    private static int[] MOVE_Y = {-1, 1, -2, 2, -2, 2, -1, 1};
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine());
+
+        StringTokenizer st;
+        StringBuilder sb = new StringBuilder();
+        while (t-- > 0) {
+            int n = Integer.parseInt(br.readLine());
+            st = new StringTokenizer(br.readLine());
+            int sx = Integer.parseInt(st.nextToken());
+            int sy = Integer.parseInt(st.nextToken());
+
+            st = new StringTokenizer(br.readLine());
+            int ex = Integer.parseInt(st.nextToken());
+            int ey = Integer.parseInt(st.nextToken());
+
+            sb.append(getShortestMoveCount(n, sx, sy, ex, ey)).append("\n");
+        }
+
+        System.out.println(sb);
+    }
+
+    private static int getShortestMoveCount(int n, int sx, int sy, int ex, int ey) {
+        if (sx == ex && sy == ey) return 0;
+
+        int[][] board = new int[n][n];
+        for (int i=0; i<n; i++) {
+            Arrays.fill(board[i], Integer.MAX_VALUE);
+        }
+
+        xQueue.add(sx);
+        yQueue.add(sy);
+        board[sx][sy] = 0;
+        while (!xQueue.isEmpty() && !yQueue.isEmpty()) {
+            int cx = xQueue.poll();
+            int cy = yQueue.poll();
+            int nextCnt = board[cx][cy]+1;
+
+            if (board[ex][ey] <= nextCnt) continue;
+
+            for (int i=0; i<MOVE_X.length; i++) {
+                int nx = cx + MOVE_X[i];
+                int ny = cy + MOVE_Y[i];
+                if (nx >= 0 && nx < n && ny >= 0 && ny < n && board[nx][ny] > nextCnt) {
+                    board[nx][ny] = nextCnt;
+
+                    if (nx != ex || ny != ey) {
+                        xQueue.add(nx);
+                        yQueue.add(ny);
+                    }
+                }
+            }
+        }
+
+        return board[ex][ey];
+    }
+}
